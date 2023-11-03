df2e5317a80503f335bb05bef8bb33ff41ff5a6d
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-11-03 09:16:07 -0500

    feat: :sparkles: Add autoupdate when comment post or delete

--- app/_components/courses/comment-box.tsx
+++ app/_components/courses/comment-box.tsx
@@ -1,16 +1,49 @@
+'use client'
 import { Button } from "@nextui-org/button"
 import { User} from "@nextui-org/user"
+import { useAuth } from '@clerk/nextjs'
+import { createBrowserClient } from "@supabase/ssr"
+import { createClient } from '@supabase/supabase-js';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/dropdown"
 import { HiArrowUturnLeft, HiEllipsisHorizontal, HiOutlinePencilSquare, HiOutlineTrash } from "react-icons/hi2"
 
 type CommentType = {
+  commentId?: string,
+  setCommentsData?: (value: any) => void,
   authorName: string,
   authorImage: string,
   date: string,
-  content: string
+  content: string,
+  isDeletable?: boolean
 }
 
-const CommentBox = ({authorName, authorImage, date, content}: CommentType) => {
+const CommentBox = ({commentId, setCommentsData, authorName, authorImage, date, content, isDeletable}: CommentType) => {
+  
+  const { getToken } = useAuth();
+  
+  const confirmDelete = async (id: string | undefined) => {
+    const supabaseAccessToken = await getToken({
+      template: "supabase",
+    });
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
+      }
+    )
+    const ok = window.confirm("Delete comment?");
+    if (ok) {
+      const { error } = await supabase.from("comments").delete().match({ id });
+      if (!error) {
+        setCommentsData && setCommentsData((prev: any) => prev.filter((comment: any) => comment.id !== id));
+        window.alert("Deleted Comment :)");
+      } else {
+        window.alert(error?.message);
+      }
+    }
+  };
+
   return (
     <article className="p-6 text-base border-b border-b-foreground/20 mb-4">
       <div className="flex justify-between items-center mb-2">
@@ -24,34 +57,52 @@ const CommentBox = ({authorName, authorImage, date, content}: CommentType) => {
             }}
           />
         </div>
-        <Dropdown>
-          <DropdownTrigger>
-            <Button 
-              variant="flat"
-              isIconOnly
-              startContent={<HiEllipsisHorizontal />}
-            />
-          </DropdownTrigger>
-          <DropdownMenu
-            aria-label="Static Actions"
-            disabledKeys={["edit"]}
-          >
-            <DropdownItem
-              key="edit"
-              startContent={<HiOutlinePencilSquare />}
+        {
+          isDeletable &&
+          <Dropdown>
+            <DropdownTrigger>
+              <Button 
+                variant="flat"
+                isIconOnly
+                startContent={<HiEllipsisHorizontal />}
+              />
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Static Actions"
+              disabledKeys={["edit"]}
             >
-              Edit
-            </DropdownItem>
-            <DropdownItem
-              key="delete"
-              className="text-danger"
-              color="danger"
-              startContent={<HiOutlineTrash />}
-            >
-              Delete
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+              <DropdownItem
+                key="edit"
+                >
+                <Button
+                  variant="flat"
+                  className="w-full"
+                  size="sm"
+                  startContent={<HiOutlinePencilSquare />}
+                  isDisabled
+                  onPress={() => console.log('edit')}
+                >  
+                  Edit
+                </Button>
+              </DropdownItem>
+              <DropdownItem
+                key="delete"
+                className="text-danger"
+                color="danger"
+                >
+                <Button
+                  variant="flat"
+                  className="w-full"
+                  size="sm"
+                  startContent={<HiOutlineTrash />}
+                  onPress={() => confirmDelete(commentId)}
+                >
+                Delete
+                </Button>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        }
       </div>
       <p className="text-gray-500 dark:text-gray-400">
         {content}
--- app/_components/courses/comments-section.tsx
+++ app/_components/courses/comments-section.tsx
@@ -10,6 +10,7 @@ import { Card, CardBody } from "@nextui-org/card"
 import { siteContent } from "@/app/_config/content";
 import { Tables, DbResult } from "@/app/_types/supabase";
 import { createBrowserClient } from '@supabase/ssr';
+import { Clerk } from '@clerk/backend';
 
 const CommentsSection = () => {
   const [commentsData, setCommentsData] = useState<Tables<'comments'>[] | null>(null)
@@ -20,6 +21,7 @@ const CommentsSection = () => {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+  const clerk = Clerk({ apiKey: '...' });
   useEffect(() => {
     const getCommentsData = async () => {
       const query = supabase.from('classes').select(`
@@ -29,8 +31,14 @@ const CommentsSection = () => {
       const { data: classInfo }: DbResult<typeof query> = await query
       classInfo && setCommentsData(classInfo[0].comments)
       classInfo && setClassID(classInfo[0].id)
+      const userList = await clerk.users.getUser('user_2XAS5JO1pblqi1ff9b5zIpIB1L0');
+      console.log(userList);
     }
+    
+
     getCommentsData()
+
+    
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedClass])
 
@@ -54,13 +62,20 @@ const CommentsSection = () => {
           { 
             (commentsData?.length) && 
             commentsData.length > 0
-            ? commentsData?.map((comment, index) => (
+            ? commentsData?.sort((a, b) => {
+              const aDate = new Date(a.created_at);
+              const bDate = new Date(b.created_at);
+              return +bDate - +aDate;
+            }).map((comment, index) => (
               <CommentBox
                 key={index}
+                setCommentsData={setCommentsData}
+                commentId={comment.id}
                 authorName={comment.user_id}
                 authorImage='https://flowbite.com/docs/images/people/profile-picture-2.jpg'
                 date={stringifyDate(comment.created_at)}
                 content={comment.content}
+                isDeletable={comment.user_id === comment.user_id}
               />
             ))
             : <div className="text-center">
